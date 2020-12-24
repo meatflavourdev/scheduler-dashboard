c737e599c61a0b4d27c923d3314020fb71a237a7
Jeremy Dombrowski <jeremy@meatflavour.dev>
2020-12-23 19:15:20 -0800

    Initial Panels and focused state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,63 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import classnames from "classnames";
+import classnames from 'classnames';
+import Loading from './Loading';
+import Panel from './Panel';
+
+const data = [
+  {
+    id: 1,
+    label: 'Total Interviews',
+    value: 6,
+  },
+  {
+    id: 2,
+    label: 'Least Popular Time Slot',
+    value: '1pm',
+  },
+  {
+    id: 3,
+    label: 'Most Popular Day',
+    value: 'Wednesday',
+  },
+  {
+    id: 4,
+    label: 'Interviews Per Day',
+    value: '2.3',
+  },
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: false,
+    focused: null,
+  };
 
-    return <main className={dashboardClasses} />;
+
+  selectPanel = function (id) {
+    this.setState((prev) => ({
+      focused: prev.focused === id ? null : id,
+    }));
+  };
+
+  render() {
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused,
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return (
+      <main className={dashboardClasses}>
+        {data
+          .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+          .map(panel => (
+            <Panel onSelect={e => this.selectPanel(panel.id)} {...panel} />
+          ))}
+      </main>
+    );
   }
 }
 
